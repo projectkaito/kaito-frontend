03a59e1ff51e9a55e6469162c627da88d3621fdc
abdulrafaysiddiqui <abdul.rafay.siddique@gmail.com>
2022-08-17 16:47:24 +0500

    renamed whitelist page to minting

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -12,7 +12,6 @@ import zIndex from "@mui/material/styles/zIndex";
 import { Theme } from "@mui/material";
 import Animation from "src/assets/images/gif.webm";
 import Minting from "./pages/Minting/Minting";
-import Whitelist from "./pages/Whitelist/Whitelist";
 import NFTPage from "./pages/NFTPage/NFTPage";
 import Test from "./pages/Test/Test";
 
@@ -103,9 +102,8 @@ const Routes: React.FC<Props> = () => {
             </>
           }
         /> */}
-        <Route path="/" element={<Whitelist />} />
+        <Route path="/" element={<Minting />} />
         <Route path="/minting" element={<Minting />} />
-        <Route path="/whitelist" element={<Whitelist />} />
         <Route path="/nft/:address/:tokenId" element={<NFTPage />} />
         <Route path="/test" element={<Test />} />
         <Route path="*" element={<NotFound />} />
