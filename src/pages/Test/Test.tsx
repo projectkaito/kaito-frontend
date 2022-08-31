7d8f607e6fc68e81bb7d3f7d5996128e04330912
Saqlain <saqlainprinters@gmail.com>
2022-08-31 14:35:26 +0500

    Added test whitelist

--- src/pages/Test/Test.tsx
+++ src/pages/Test/Test.tsx
@@ -1,18 +1,35 @@
+// @ts-nocheck
 import React from "react";
 import { makeStyles } from "@mui/styles";
-import { Button, Card, MenuItem, Paper, TextField, Theme } from "@mui/material";
+import {
+  Button,
+  Card,
+  MenuItem,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+  Theme,
+} from "@mui/material";
 import Img from "src/assets/images/nfts/1.jpg";
 import { NFT_IMAGES } from "src/config/constants";
 import useWallet from "src/hooks/useWallet";
 import { WhitelistUserType } from "src/types/apis";
-import { addToWhitelist, removefromWhitelist } from "src/api/whitelist";
+import { addToWhitelist, getWhitelistInfo, removefromWhitelist } from "src/api/whitelist";
 import useNotify from "src/hooks/useNotify";
+import Bg from "src/assets/images/bg2.png";
 
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
     position: "relative",
     paddingTop: 50,
     paddingLeft: 50,
+    background: `url(${Bg})`,
+    backgroundSize: "cover",
     paddingRight: 50,
   },
   paper: {
@@ -27,64 +44,41 @@ const Test: React.FC<Props> = () => {
   const classes = useStyles();
   const { account } = useWallet();
   const [acc, setAcc] = React.useState(account);
-  const [qty, setQty] = React.useState(1);
-  const [type, setType] = React.useState<WhitelistUserType>(WhitelistUserType.Whitelist);
   const { notifyError, notifySuccess } = useNotify();
+  const [data, setData] = React.useState<any>({});
 
-  const add = async () => {
-    if (!acc) return;
-    let res = await addToWhitelist(acc, qty, type);
-    if (!res.status) {
-      notifyError("Error", "Something went wrong!");
-    } else {
-      notifySuccess("Success", res.message);
-    }
-  };
-
-  const remove = async () => {
-    if (!acc) return;
-    let res = await removefromWhitelist(acc);
-    if (!res.status) {
-      notifyError("Error", "Something went wrong!");
-    } else {
-      notifySuccess("Success", res.message);
-    }
+  const checkWhitelist = () => {
+    getWhitelistInfo(acc).then((res) => {
+      setData(res);
+    });
   };
 
   return (
     <div className={classes.root}>
       <Paper className={classes.paper}>
         <TextField fullWidth label="Address" value={acc} onChange={(e) => setAcc(e.target.value)} sx={{ mb: 2 }} />
-        <TextField
-          fullWidth
-          label="Quantity"
-          value={qty}
-          onChange={(e) => setQty(Number(e.target.value))}
-          type="number"
-          sx={{ mb: 2 }}
-        />
-        <TextField
-          fullWidth
-          label="Type"
-          value={type}
-          onChange={(e) => setType(e.target.value as WhitelistUserType)}
-          select
-          variant="standard"
-          sx={{ mb: 2 }}
-        >
-          {Object.values(WhitelistUserType).map((type) => (
-            <MenuItem value={type} key={type}>
-              {type}
-            </MenuItem>
-          ))}
-        </TextField>
 
-        <Button variant="outlined" sx={{ m: 1 }} onClick={add}>
-          Add To Whitelist
+        <Button variant="outlined" sx={{ m: 1 }} onClick={checkWhitelist}>
+          Check Whitelist
         </Button>
-        <Button variant="outlined" sx={{ m: 1 }} onClick={remove}>
-          Remove From Whitelist
-        </Button>
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Key</TableCell>
+                <TableCell>Value</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {Object.entries(data).map(([key, value]) => (
+                <TableRow>
+                  <TableCell>{key.toString()}</TableCell>
+                  <TableCell>{value}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Paper>
     </div>
   );
