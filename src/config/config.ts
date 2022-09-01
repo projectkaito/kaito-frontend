98e164a332411d22677c61c8274a9703617263c7
abdulrafaysiddiqui <abdul.rafay.siddique@gmail.com>
2022-09-01 18:37:44 +0500

    updated api to heroku

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 export const BASE_API_URL = "";
 export const isProduction = process.env.REACT_APP_PRODUCTION === "true";
-export const WHITELIST_API_URL = "http://kaito-env.eba-petzbkwv.us-east-1.elasticbeanstalk.com/api";
+export const WHITELIST_API_URL = "https://projectkaito-api.herokuapp.com/api";
 export const ERC721_INTERFACE = "0x80ac58cd";
 export const ERC1155_INTERFACE = "0xd9b67a26";
 
